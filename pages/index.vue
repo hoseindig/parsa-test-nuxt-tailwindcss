97e602ae8abebd1f4a7f5479dd44d79a12796de3
hoseinx <hoseinx@gmail.com>
2025-07-21 12:59:50 +0330

    base config

---
+++ pages/index.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="min-h-screen flex items-center justify-center bg-gray-100">
+    <div class="text-center">
+      <h1 class="text-4xl font-bold text-blue-600 mb-4">خوش آمدید به پروژه Nuxt + Tailwind</h1>
+      <p class="text-lg text-gray-700">این یک پروژه نمونه است که با Nuxt 3 و Tailwind CSS ساخته شده است.</p>
+    </div>
+  </div>
+</template>
