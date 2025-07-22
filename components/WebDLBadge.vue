94dbcb42fcdbb93bdb1684b36b9b4c858bd39f6e
hoseinx <hoseinx@gmail.com>
2025-07-22 12:15:22 +0330

    WebDLBadge

---
+++ components/WebDLBadge.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="inline-block bg-red-600 rounded-lg p-1 shadow-lg max-w-fit ml-4">
+    <!-- Top section with 1080 -->
+    <div
+      class="bg-gray-100 text-red-600 font-bold text-2xl px-4 py-2 text-center rounded-t-md"
+    >
+      1080
+    </div>
+
+    <!-- Bottom section with WebDL -->
+    <div class="text-white font-bold text-lg px-4 py-2 text-center">WebDL</div>
+  </div>
+</template>
+
+}
